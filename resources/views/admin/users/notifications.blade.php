d96f163af8738bb9c78770a5309cd79b29204379
FERIAL HADJADJ <FERIAL.HADJADJ@ad.poste.dz>
2026-02-13 16:49:37 +0100

    notification fix

--- resources/views/admin/users/notifications.blade.php
+++ resources/views/admin/users/notifications.blade.php
@@ -63,7 +63,7 @@
                         <div class="col-md-3"> <span class="badge badge-primary badge-pill   "> {{$notification->data['type']}} </span></div>
                         <div class="col-md-6">{{$notification->data['text']}}</div>
                         <div class="col-md-2"><span class="badge badge-warning badge-pill float-right">{{$notification->created_at->diffForHumans()}}</span> </div>
-                        <div class="col-md-1"><span class=" float-right"><input type="checkbox" id="{{$notification->id}} name="Read" value="{{$notification->id}}"></span> </div>
+                      <div class="col-md-1"><span class=" float-right"><input type="checkbox" id="{{$notification->id}} name="Read" value="{{$notification->id}}"></span> </div>
 
                     </div>
                 </a>
